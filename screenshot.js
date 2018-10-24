bdf2ecb394761aac49e1c69bb3908b5fc135d3a1
Wadah Esam <wadah-wleed@hotmail.com>
2018-10-24 17:53:50 +0300

    Initial commit

---
+++ screenshot.js
@@ -0,0 +1,13 @@
+const puppeteer = require('puppeteer');
+
+(async () => {
+  const browser = await puppeteer.launch({headless: false});
+  const page = await browser.newPage();
+  await page.setViewport({height: 1080, width: 1920});
+  await page.goto('https://fadfadah.net');
+  await page.waitFor(1000);
+  await page.screenshot({path: 'fadfadah.png'});
+  await browser.close();
+})();
+
+
